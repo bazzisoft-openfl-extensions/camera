c44e13f41848d615a901265719b7a6feeb98c253
Macca <macca@calmcreek.com>
2017-04-28 15:54:15 +0100

    Modifying to work with OpenFL 4.9.2.

--- camera/utils/ImageUtils.hx
+++ camera/utils/ImageUtils.hx
@@ -1,13 +1,13 @@
 package camera.utils;
 import flash.display.BitmapData;
-#if flash11_3
-import flash.display.JPEGEncoderOptions;
-import flash.display.PNGEncoderOptions;
-#end
 import flash.geom.Rectangle;
 import flash.utils.ByteArray;
 import haxe.io.Bytes;
 
+#if (!flash || flash11_3)
+import flash.display.JPEGEncoderOptions;
+import flash.display.PNGEncoderOptions;
+#end
 
 class ImageUtils
 {
@@ -15,37 +15,29 @@ class ImageUtils
     public static inline var FORMAT_PNG : String = "png";
     
     public static function ConvertBitmapDataToImageData(bitmapData:BitmapData, format:String, quality:Float = 0.9) : Bytes
-    {        
-        #if flash11_3
-        
+    {
+        #if (!flash || flash11_3)
         var options:Dynamic = (format == FORMAT_JPEG ? new JPEGEncoderOptions(Std.int(quality * 100.0)) : new PNGEncoderOptions());
         var imageData:ByteArray = bitmapData.encode(new Rectangle(0, 0, bitmapData.width, bitmapData.height), options);
         return Bytes.ofData(imageData);
-        
-        #elseif flash
-        
-        throw "Unable to convert from BitmapData to JPEG/PNG Bytes in Flash < 11.3. Try <app swf-version=\"11.3\"/>";
-        
-        #else
-        
-        var imageData:ByteArray = bitmapData.encode(format, quality);
-        return cast(imageData, Bytes);
-        
+
         #end
+
+        throw "Unable to convert from BitmapData to JPEG/PNG Bytes in Flash < 11.3. Try <app swf-version=\"11.3\"/>";
     }
     
     public static function ConvertImageDataToBitmapData(imageData:Bytes) : BitmapData
     {
-        #if flash        		
-		var bytes:ByteArray = imageData.getData();
+        #if flash
+        var bytes:ByteArray = imageData.getData();
         #else        
         var bytes:ByteArray = ByteArray.fromBytes(imageData);
         #end        
-		
+
         #if flash
         throw "Unable to convert from JPEG Bytes to BitmapData synchronously in flash.";
         #else
-        return BitmapData.loadFromBytes(bytes);
+        return BitmapData.loadFromBytes(bytes).result();
         #end
     }
 }
